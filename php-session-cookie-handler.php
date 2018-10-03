f1058fc4eb645064bb0ea5c3052a5dabebeea90a
Yasuo Ohgaki <yohgaki@php.net>
2018-10-03 10:23:20 +0900

    Make input validation complete. In return, occational lost cookie data mitigation is removed.

--- php-session-cookie-handler.php
+++ php-session-cookie-handler.php
@@ -30,6 +30,9 @@ class SessionCookie implements SessionHandlerInterface{
     // e.g. Random key like bin2hex(random_bytes(32));
     private $key;
 
+    // Cookies used. These are validated with psvk
+    private $cookies = ['psdat', 'psct', 'pskey', 'psts', 'psiv'];
+
     // Cookie params
     private $cookie_lifetime;
     private $cookie_path;
@@ -48,13 +51,13 @@ class SessionCookie implements SessionHandlerInterface{
     const TAMPER = true;
 
     function __construct($key = null, $use_exception = true) {
+        assert(is_null($key) || (is_string($key) && strlen($key) > 20));
         assert(is_bool($use_exception));
 
-        if ($key) {
-            $this->key = $key;
-        } else if (isset($_ENV['PHP_SESSION_COOKIE_KEY'])) {
-            $this->key = $_ENV['PHP_SESSION_COOKIE_KEY'];
-        } else {
+        $env_key = getenv('PHP_SESSION_COOKIE_KEY');
+        assert(is_null($env_key) || (is_string($env_key) && strlen($env_key) > 20));
+        $this->key = $key ?? $env_key ?? null;
+        if (!$this->key) {
             throw new Exception('Cookie based session cannot work without secret master key.');
         }
 
@@ -96,7 +99,7 @@ class SessionCookie implements SessionHandlerInterface{
         }
 
         if ($_COOKIE['psts'] + $this->gc_maxlifetime < time()) {
-            $this->resetCookie(true);
+            $this->resetCookie();
             return '';
         }
 
@@ -113,10 +116,11 @@ class SessionCookie implements SessionHandlerInterface{
         if (strlen($data) > 2048) {
             trigger_error('You cannot save too large session data over 2KB.');
             $this->resetCookie();
+            $this->setDataCookie('');
             return true;
         }
         $edata = $this->encrypt($data);
-        $this->setcookie('psdat', $edata);
+        $this->setDataCookie($edata);
         return true;
     }
 
@@ -124,6 +128,7 @@ class SessionCookie implements SessionHandlerInterface{
     function destroy($sid)
     {
         $this->resetCookie(true);
+        $this->setDataCookie('');
     }
 
 
@@ -151,12 +156,10 @@ class SessionCookie implements SessionHandlerInterface{
 
         if ($_COOKIE['psts'] + self::RENEW < time()) {
             $ts = (string)time();
-            $this->setcookie('psts2', $_COOKIE['psts']);
             $this->setcookie('psts', $ts);
-            $_COOKIE['psts2'] = $_COOKIE['psts'];
             $_COOKIE['psts'] = $ts;
             $edata = $this->encrypt($data);
-            $this->setcookie('psdat', $edata);
+            $this->setDataCookie($edata);
         }
         return true;
     }
@@ -174,6 +177,8 @@ class SessionCookie implements SessionHandlerInterface{
 
     private function detectedAttack($error_msg)
     {
+        $this->resetCookie(true);
+        $this->setDataCookie('');
         if (!self::TAMPER) {
             return;
         }
@@ -182,17 +187,6 @@ class SessionCookie implements SessionHandlerInterface{
         } else {
             trigger_error($error_msg);
         }
-    }
-
-
-    private function base64_decode($data)
-    {
-        $tmp = base64_decode($data);
-        if ($tmp === false) {
-            $this->detectedAttack('Attack detected: Invalid base64 data.');
-            return '';
-        }
-        return $tmp;
     }
 
 
@@ -214,31 +208,13 @@ class SessionCookie implements SessionHandlerInterface{
     }
 
 
-    private function getPreviousEnckey()
-    {
-        assert(isset($_COOKIE['psct']));
-        assert(isset($_COOKIE['psts2']));
-        assert(isset($_COOKIE['pskey2']));
-
-        $ct  = $_COOKIE['psct'] ?? 0;
-        $ts  = $_COOKIE['psts2'] ?? 0;
-        $key = $_COOKIE['pskey2'] ?? '';
-        $enckey = hash_hkdf('sha256',
-                            $this->key,
-                            0,
-                            $ts.'&'.$ct,
-                            $key);
-        return $enckey;
-    }
-
-
     private function encrypt($data)
     {
         assert(isset($_COOKIE['psdat']));
         assert(isset($_COOKIE['psiv']));
 
         $enckey = $this->getEnckey();
-        $iv     = $this->base64_decode($_COOKIE['psiv']);
+        $iv     = base64_decode($_COOKIE['psiv']);
         $edata  = openssl_encrypt($data, 'AES-256-CBC', $enckey, 0, $iv);
 
         if ($edata === false) {
@@ -253,31 +229,17 @@ class SessionCookie implements SessionHandlerInterface{
         assert(isset($_COOKIE['psdat']));
         assert(isset($_COOKIE['psiv']));
 
-        $data   = $this->base64_decode($_COOKIE['psdat']);
+        $data   = base64_decode($_COOKIE['psdat']);
         if (!$data) {
             return '';
         }
 
         $enckey = $this->getEnckey();
-        $iv     = $this->base64_decode($_COOKIE['psiv']);
+        $iv     = base64_decode($_COOKIE['psiv']);
         $sdata  = openssl_decrypt($data, 'AES-256-CBC', $enckey, 0, $iv);
         if ($sdata !== false) {
             return $sdata;
         }
-
-        if (empty($_COOKIE['psiv2'])) {
-            $this->detectedAttack('Attack detected: No psiv2.');
-            return '';
-        }
-
-        $enckey = $this->getPreviousEnckey();
-        $iv     = $this->base64_decode($_COOKIE['psiv2']);
-        $sdata  = openssl_decrypt($data, 'AES-256-CBC', $enckey, 0, $iv);
-        if ($sdata !== false) {
-            return $sdata;
-        }
-        $this->resetCookie(true);
-        $this->detectedAttack('Attack detected: Tampered key info.');
         return '';
     }
 
@@ -287,22 +249,23 @@ class SessionCookie implements SessionHandlerInterface{
         if (empty($_COOKIE['pskey']) ||
             empty($_COOKIE['psiv']) ||
             empty($_COOKIE['psts']) ||
-            !isset($_COOKIE['psdat'])
+            empty($_COOKIE['psct']) ||
+            empty($_COOKIE['psdat']) ||
+            empty($_COOKIE['psvk'])
         ) {
-            $this->resetCookie();
+            $this->resetCookie(true);
             return false;
         }
 
-        if (strlen($_COOKIE['pskey']) != 44 ||
-            strlen($_COOKIE['psiv']) != 24 ||
-            strlen($_COOKIE['psts']) < 10
-        ) {
-            $this->resetCookie();
+        foreach ($this->cookies as $c) {
+            $tmp[] = $_COOKIE[$c];
+        }
+        $cookies = join('&', $tmp);
+        if (!hash_equals($_COOKIE['psvk'], hash_hmac('sha256', $cookies, $this->key))) {
             $this->detectedAttack('Attack detected: Invalid key info.');
             return false;
         }
 
-        $this->validateCookieTimestamp();
         $maxlifetime = ini_get('session.gc_maxlifetime');
         if ($_COOKIE['psts'] + $maxlifetime < time()) {
             $this->resetCookie();
@@ -313,66 +276,19 @@ class SessionCookie implements SessionHandlerInterface{
     }
 
 
-    private function resetCookie($clear_all = false)
+    private function resetCookie($clear_ct = false)
     {
-        $this->validateCookieTimestamp();
-
-        if ($clear_all) {
-            $this->setcookie('pskey2',  'na');
-            $this->setcookie('psiv2',   'na');
-            $this->setcookie('psts2',   0);
-            $_COOKIE['pskey2'] = 'na';
-            $_COOKIE['psiv2']  = 'na';
-            $_COOKIE['psts2']  = 0;
-        } else {
-            $this->setcookie('pskey2',  $_COOKIE['pskey'] ?? 'na');
-            $this->setcookie('psiv2',   $_COOKIE['psiv'] ?? 'na');
-            $this->setcookie('psts2',   $_COOKIE['psts'] ?? 0);
-            $_COOKIE['pskey2'] = $_COOKIE['pskey'] ?? 'na';
-            $_COOKIE['psiv2']  = $_COOKIE['psiv'] ?? 'na';
-            $_COOKIE['psts2']  = $_COOKIE['psts'] ?? 0;
-        }
-
-        $this->setBaseCookie();
+        $this->setBaseCookie($clear_ct);
     }
 
 
     private function updateCookie()
     {
-        assert(isset($_COOKIE['pskey']));
-        assert(isset($_COOKIE['psiv']));
-        assert(isset($_COOKIE['psts']));
-        assert(isset($_COOKIE['pskey2']));
-        assert(isset($_COOKIE['psiv2']));
-        assert(isset($_COOKIE['psts2']));
-
-        $this->validateCookieTimestamp();
-
-        $this->setcookie('pskey2',  $_COOKIE['pskey']); // HKDF derivation key (salt)
-        $this->setcookie('psiv2',   $_COOKIE['psiv']);
-        $this->setcookie('psts2',   $_COOKIE['psts']);
-        $_COOKIE['pskey2'] = $_COOKIE['pskey'];
-        $_COOKIE['psiv2']  = $_COOKIE['psiv'];
-        $_COOKIE['psts2']  = $_COOKIE['psts'];
-
-        $this->setBaseCookie();
+        $this->setBaseCookie(false);
     }
 
 
-    private function validateCookieTimestamp()
-    {
-        if (isset($_COOKIE['psts']) && $_COOKIE['psts'] -1 > time()) {
-            $this->resetCookie(true);
-            $this->detectedAttack('Attack detected: psts has future timestamp.');
-        }
-        if (isset($_COOKIE['psts2']) && $_COOKIE['psts2'] - 1> time()) {
-            $this->resetCookie(true);
-            $this->detectedAttack('Attack detected: psts2 has future timestamp.');
-        }
-    }
-
-
-    private function setBaseCookie()
+    private function setBaseCookie($clear_ct)
     {
         $key = base64_encode(random_bytes(32));
         $iv  = base64_encode(random_bytes(16));
@@ -385,10 +301,27 @@ class SessionCookie implements SessionHandlerInterface{
         $_COOKIE['psiv']  = $iv;
         $_COOKIE['psts']  = $ts;
         $_COOKIE['psdat'] = '';
-        if (empty($_COOKIE['psct'])) {
+        if ($clear_ct || empty($_COOKIE['psct'])) {
             $this->setcookie('psct',   $ts);
             $_COOKIE['psct']  = $ts;
         }
     }
 
+
+    private function setDataCookie($edata) {
+        assert(isset($_COOKIE['psct']));
+        assert(isset($_COOKIE['pskey']));
+        assert(isset($_COOKIE['psiv']));
+        assert(isset($_COOKIE['psts']));
+
+        $this->setcookie('psdat', $edata);
+        $_COOKIE['psdat'] = $edata;
+        foreach($this->cookies as $c) {
+            $tmp[] = $_COOKIE[$c];
+        }
+        $cookies = join('&', $tmp);
+        $vk = hash_hmac('sha256', $cookies, $this->key);
+        $this->setcookie('psvk', $vk);
+        $_COOKIE['psvk'] = $vk;
+    }
 }
